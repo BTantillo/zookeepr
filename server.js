54acd58adec436289d933799e749058db6853e8c
Mountaineer Ben <bentantillo@gmail.com>
2021-10-12 18:25:57 -0700

    POST server response call

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     let filteredResults = animalsArray;
@@ -45,6 +51,10 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    console.log(body);
+}
+
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -63,9 +73,16 @@ app.get('/api/animals', (req, res) => {
       }
   })
 
+  app.post('/api/animals', (req, res) => {
+        // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
+  });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 })
+
+
 
 
