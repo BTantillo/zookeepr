735b9f513e52f5af5d93ee15013199f2cf149311
Mountaineer Ben <bentantillo@gmail.com>
2021-10-13 15:06:01 -0700

    test created, as well as some sepate pages

---
+++ __tests__/zookeepers.test.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const {
+  filterByQuery,
+  findById,
+  createNewZookeeper,
+  validateZookeeper,
+} = require("../lib/zookeepers.js");
+const { zookeepers } = require("../data/zookeepers");
+
+
+
